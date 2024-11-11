4a7b97436b920d48e3bcebe80fe88b4d78f95661
Turgunoff <eldor.turgunov.94@yandex.ru>
2024-11-11 13:53:41 +0500

    new

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:math_kids/logic/providers/plus_provider.dart';
+import 'package:math_kids/ui/screens/splash_screen/splash_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'logic/providers/loading_provider.dart';
+
+///provider
+///likes: 9562
+///pub points: 140
+///popularity: 100
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => LoadingProvider()),
+          ChangeNotifierProvider(create: (_) => PlusProvider()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              primarySwatch: Colors.teal,
+              appBarTheme: const AppBarTheme(centerTitle: true)),
+          home: const SplashScreen(),
+        ));
+  }
+}
